959b0df13ba5272eca602ea5ce2379e7902d6f22
barviz <barbaravizgarra@gmail.com>
2023-05-24 19:23:30 -0300

    agregando metodos a servicios

--- src/main/java/com/bv/biblioteca/services/AutorServicio.java
+++ src/main/java/com/bv/biblioteca/services/AutorServicio.java
@@ -2,8 +2,13 @@ package com.bv.biblioteca.services;
 
 import com.bv.biblioteca.models.Autor;
 import com.bv.biblioteca.repositories.AutorRepositorio;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AutorServicio {
@@ -11,6 +16,7 @@ public class AutorServicio {
     @Autowired
     private AutorRepositorio autorRepositorio;
 
+    @Transactional
     public void crearAutor(String nombre){
 
         Autor autor = new Autor();
@@ -19,5 +25,29 @@ public class AutorServicio {
 
         autorRepositorio.save(autor);
 
+    }
+
+    public List<Autor> listarAutores(){
+
+        List<Autor> autores = new ArrayList<>();
+
+        autores = autorRepositorio.findAll();
+
+        return autores;
+    }
+
+    @Transactional
+    public void actualizarAutor(String id, String nombre) {
+
+        Optional<Autor> respuesta = autorRepositorio.findById(id);
+
+        if (respuesta.isPresent()) {
+
+            Autor autor = respuesta.get();
+
+            autor.setNombre(nombre);
+
+            autorRepositorio.save(autor);
+        }
     }
 }
--- src/main/java/com/bv/biblioteca/services/EditorialServicio.java
+++ src/main/java/com/bv/biblioteca/services/EditorialServicio.java
@@ -1,10 +1,16 @@
 package com.bv.biblioteca.services;
 
 
+import com.bv.biblioteca.models.Autor;
 import com.bv.biblioteca.models.Editorial;
 import com.bv.biblioteca.repositories.EditorialRepositorio;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EditorialServicio {
@@ -12,6 +18,7 @@ public class EditorialServicio {
     @Autowired
     private EditorialRepositorio editorialRepositorio;
 
+    @Transactional
     public void crearEditorial(String nombre){
 
         Editorial editorial = new Editorial();
@@ -19,6 +26,32 @@ public class EditorialServicio {
         editorial.setNombre(nombre);
 
         editorialRepositorio.save(editorial);
+
+    }
+
+    public List<Editorial> listarEditoriales(){
+
+        List<Editorial> editoriales = new ArrayList<>();
+
+        editoriales = editorialRepositorio.findAll();
+
+        return editoriales;
+    }
+
+    @Transactional
+    public void actualizarEditorial(String id, String nombre) {
+
+        Optional<Editorial> respuesta = editorialRepositorio.findById(id);
+
+        if (respuesta.isPresent()) {
+
+            Editorial editorial = respuesta.get();
+
+            editorial.setNombre(nombre);
+
+            editorialRepositorio.save(editorial);
+
+        }
 
     }
 }
