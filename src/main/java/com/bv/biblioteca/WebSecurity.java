e452369c265af901cca71f5714f60c3b9b2da618
barviz <barbaravizgarra@gmail.com>
2023-05-28 22:21:02 -0300

    agregando websecuritycustomizer bean

--- src/main/java/com/bv/biblioteca/WebSecurity.java
+++ src/main/java/com/bv/biblioteca/WebSecurity.java
@@ -2,23 +2,15 @@ package com.bv.biblioteca;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurity {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests((authz) -> authz
-                        .anyRequest().authenticated()
-                )
-                .httpBasic(withDefaults());
-        return http.build();
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web -> web.ignoring().requestMatchers("/css/*","/js/*","/img/*","/**"));
     }
 }
